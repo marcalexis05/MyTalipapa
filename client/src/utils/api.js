860e00588e50803df5ce0b625f85a5682d14263b
Christian Dave Juliales <dave.juliales@gmail.com>
2026-05-31 08:20:53 +0800

    sana

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,2 +1,1 @@
-// client/src/utils/api.js (or wherever you call fetch)
 const API_BASE = import.meta.env.VITE_BACKEND_URL || '/api';
--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const app = require('../src/index.js');
 
 module.exports = app;
