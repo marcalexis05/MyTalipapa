8248ef6c9f7d3689648f4d1920b2915466a80708
Christian Dave Juliales <dave.juliales@gmail.com>
2026-06-02 05:24:55 +0800

    ito

--- client/src/pages/Admin/AdminApplication.jsx
+++ client/src/pages/Admin/AdminApplication.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { ChevronRight, Store } from "lucide-react";
-import { useCurrentUser } from '../../utils/auth';
+import { useCurrentUser, getToken } from '../../utils/auth';
 import AdminSidebar from '../../components/AdminSidebar';
 import NotificationBell from '../../components/NotificationBell';
-import { useCurrentUser, getToken } from '../../utils/auth';
 
 const NAV_ITEMS = [
   {
