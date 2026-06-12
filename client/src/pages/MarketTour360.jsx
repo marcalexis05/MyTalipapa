9d8b060d4b9a990df21599f567e599c777f2f9dc
veryvin <earvinjohnlopez01@gmail.com>
2026-06-12 08:35:07 +0800

    test

--- client/src/pages/MarketTour360.jsx
+++ client/src/pages/MarketTour360.jsx
@@ -695,7 +695,7 @@ export default function MarketTour360() {
 
     if (SVG_STALL_COORDS[rawKey]) {
       x = SVG_STALL_COORDS[rawKey].x;
-      y = SVG_STALL_COORDS[rawKey].y + yOffset;
+      y = SVG_STALL_COORDS[rawKey].y;
     } else if (SVG_STALL_COORDS[cleanKey]) {
       x = SVG_STALL_COORDS[cleanKey].x;
       y = SVG_STALL_COORDS[cleanKey].y + yOffset;
